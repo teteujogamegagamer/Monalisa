23f5677efe6352f0e1d5a992010d43213cc93728
Fã do Manoel Gomes <00001125270548SP@al.educacao.sp.gov.br>
2025-04-02 12:18:39 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+let olhoX;
+let olhoY;
+
+function draw() {
+  background("#6B17EC");
+  fill("#03F428");
+  circle(200, 200, 300); // rosto
+  triangle(137, 64, 94, 92, 103, 38);
+  triangle(249, 60, 285, 43, 290, 82);
+  fill("rgb(241,137,137)");
+  circle(150, 150, 60); // olho esquerdo
+  circle(250, 150, 60); // olho direito
+  line(150, 270, 250, 235); // boca
+  fill("#BA15EB");
+  triangle(200, 180, 170, 220, 220, 220); // nariz
+  line(123, 115, 178, 113); // sobrancelha esquerda
+  line(225, 116, 279, 106); // sobrancelha direita
+  // circle(150,150,10); // pupila esquerda
+  //circle(250,150,10); // pupila direita
+
+  olhoX = map(mouseX, 0, 400, 130, 170);
+  olhoY = map(mouseY, 0, 400, 130, 170);
+
+  circle(olhoX, olhoY, 10); // nova pupila esquerda
+  circle(olhoX + 100, olhoY, 10); //nova pupila direita
+  if (mouseIsPressed) {
+    console.log(mouseX, mouseY);
+  }
+}
